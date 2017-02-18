25346f822ff2935e626632b6d5d53be0298c32f1
g-serg <mr-sergey2004@yandex.ru>
2017-02-18 12:05:52 +0300

    workflow settings

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -4,7 +4,8 @@ module.exports = function() {
   $.gulp.task('serve', function() {
     $.browserSync.init({
       open: false,
-      server: $.config.root
+      server: $.config.root,
+      files: "*"
     });
 
     $.browserSync.watch([$.config.root + '/**/*.*', '!**/*.css'], $.browserSync.reload);
