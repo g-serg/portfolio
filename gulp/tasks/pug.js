f1b7ff22bbb7b747d0e616bc00ecdb529d9477a3
g-serg <mr-sergey2004@yandex.ru>
2017-02-19 23:48:37 +0300

    add pug&sass glob

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,15 +1,44 @@
 'use strict';
 
-module.exports = function() {
-  $.gulp.task('pug', function() {
-    return $.gulp.src('./source/template/pages/*.pug')
-      .pipe($.gp.pug({ pretty: true }))
-      .on('error', $.gp.notify.onError(function(error) {
-        return {
-          title: 'Pug',
-          message:  error.message
-        }
-       }))
-      .pipe($.gulp.dest($.config.root));
-  });
+module.exports = function () {
+
+    $.gulp.task('pug', function () {
+
+        var notifyGeneric = {
+            title: function () {
+                return '<%= file.relative %> - ' + this.s.prettySize;
+            },
+            onLast: true,
+            subtitle: "Successfully Compiled",
+            message: "time: <%= options.hour %>:<%= options.minute %>:<%= options.second %>",
+            templateOptions: {
+                hour: lz(new Date().getHours()),
+                minute: lz(new Date().getMinutes()),
+                second: lz(new Date().getSeconds()),
+            },
+            s: $.gp.size()
+        };
+
+        return $.gulp.src('./source/template/pages/*.pug')
+            .pipe($.gp.pug({
+                plugins: [$.pugIncludeGlob({/* options */})],
+                pretty: true,
+                locals : {
+                    init: JSON.parse($.fs.readFileSync('./source/initial.json', 'utf8')),
+                }
+            }))
+            .on('error', $.gp.notify.onError(function (error) {
+                return {
+                    title: 'Pug',
+                    message: error.message
+                }
+            }))
+            .pipe($.gulp.dest($.config.root))
+            .pipe(notifyGeneric.s)
+            .pipe($.gp.notify(notifyGeneric))
+    });
+
+    function lz(val) {
+        return ('0' + val).slice(-2);
+    }
 };
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,13 +1,37 @@
 'use strict';
 
-module.exports = function() {
-  $.gulp.task('sass', function() {
-    return $.gulp.src('./source/style/main.scss')
-      .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
-      .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
-      .pipe($.gp.sourcemaps.write())
-      .pipe($.gulp.dest($.config.root + '/assets/css'))
-      .pipe($.browserSync.stream());
-  })
+module.exports = function () {
+
+    $.gulp.task('sass', function () {
+
+        var notifyGeneric = {
+            title: function () {
+                return '<%= file.relative %> - ' + this.s.prettySize;
+            },
+            onLast: true,
+            subtitle: "Successfully Compiled",
+            message: "time: <%= options.hour %>:<%= options.minute %>:<%= options.second %>",
+            templateOptions: {
+                hour: lz(new Date().getHours()),
+                minute: lz(new Date().getMinutes()),
+                second: lz(new Date().getSeconds()),
+            },
+            s: $.gp.size()
+        };
+
+        return $.gulp.src('./source/style/main.scss')
+            .pipe($.gp.sassGlob())
+            .pipe($.gp.sourcemaps.init())
+            .pipe($.gp.sass()).on('error', $.gp.notify.onError({title: 'Style'}))
+            .pipe($.gp.autoprefixer({browsers: $.config.autoprefixerConfig}))
+            .pipe($.gp.sourcemaps.write())
+            .pipe($.gulp.dest($.config.root + '/assets/css'))
+            .pipe(notifyGeneric.s)
+            .pipe($.gp.notify(notifyGeneric))
+            .pipe($.browserSync.stream())
+    });
+
+    function lz(val) {
+        return ('0' + val).slice(-2);
+    }
 };
